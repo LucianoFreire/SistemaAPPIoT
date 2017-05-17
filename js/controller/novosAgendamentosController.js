a850cfa0b55a812d44edc7e47884e23485456faf
Thiago Oliveira <oliveira@Oliveira.Oliveira>
2017-05-16 20:04:59 -0400

    Alteracoes

--- js/controller/novosAgendamentosController.js
+++ js/controller/novosAgendamentosController.js
@@ -1,6 +1,7 @@
 angular.module("sistemaappiot").controller("novosAgendamentosController", function ($scope, sistemaApi, $location) {
     $scope.estado = {};
     $scope.data = {};
+    $scope.alertas = [];
     $scope.novoAg = function () {
         $location.url('/agendamentos');
     };
@@ -10,12 +11,16 @@ angular.module("sistemaappiot").controller("novosAgendamentosController", functi
         $scope.estado.data = moment($scope.data).format("DD/MM/YYYY");
         $scope.estado.hora = moment($scope.data).format("HH:MM:ss");
         $scope.estado = estado;
-        sistemaApi.salvarAgenda($scope.estado).then(function (dados) {
-            // alert("Salvo");
-            $scope.novoAg();
-        }, function (err) {
-            alert("Erro");
-        });
+        if($scope.estado.EstadoTelevisor !=null && $scope.estado.EstadoVentilador !=null
+            && $scope.estado.EstadoLuzQuarto != null && $scope.estado.EstadoLuzSala != null){
+            sistemaApi.salvarAgenda($scope.estado).then(function (dados) {
+                $scope.novoAg();
+            }, function (err) {
+                $scope.alertas.push("Erro ao Cadastrar");
+            });
+        }else{
+                $scope.alertas.push("Preencha os campos");
+        }
     };
    
 });
