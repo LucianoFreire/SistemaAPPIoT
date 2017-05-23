0c1262f19e3bdbe9f8dac2c4fab5517409103c48
Thiago Oliveira <oliveira@Oliveira.Oliveira>
2017-05-22 22:01:50 -0400

    novas alteracoes

--- js/controller/novosAgendamentosController.js
+++ js/controller/novosAgendamentosController.js
@@ -2,24 +2,38 @@ angular.module("sistemaappiot").controller("novosAgendamentosController", functi
     $scope.estado = {};
     $scope.data = {};
     $scope.alertas = [];
+    $scope.userOptions = [{'id': 'Ligado', 'text': 'Ligado'}, {'id': 'Desligado', 'text': 'Desligado'}];
+
     $scope.novoAg = function () {
         $location.url('/agendamentos');
     };
     $scope.data = new Date();
-
     $scope.salvar = function (estado) {
+        $scope.alertas = [];
         $scope.estado.data_hora = moment($scope.data).format("YYYY-MM-DD HH:mm:ss");
         $scope.estado = estado;
-        if($scope.estado.EstadoTelevisor !=null && $scope.estado.EstadoVentilador !=null
-            && $scope.estado.EstadoLuzQuarto != null && $scope.estado.EstadoLuzSala != null){
-            sistemaApi.salvarAgenda($scope.estado).then(function (dados) {
-                $scope.novoAg();
-            }, function (err) {
-                $scope.alertas.push("Erro ao Cadastrar");
-            });
+        var data_atual = new Date();
+        if($scope.data > data_atual){
+            if($scope.estado.EstadoTelevisor !=null && $scope.estado.EstadoVentilador !=null
+                    && $scope.estado.EstadoLuzQuarto != null && $scope.estado.EstadoLuzSala != null){
+                    sistemaApi.salvarAgenda($scope.estado).then(function (dados) {
+                        $scope.novoAg();
+                    }, function (err) {
+                        $scope.alertas.push("Erro ao Cadastrar");
+                    });
+                }else{
+                        $scope.alertas.push("Preencha os campos");
+                }
         }else{
-                $scope.alertas.push("Preencha os campos");
+            $scope.alertas.push("Data invalida! data deve ser maior que data ou a hora atual :D");
         }
     };
+    $scope.addAlert = function() {
+        $scope.alerts.push({msg: 'Another alert!'});
+  };
+
+    $scope.closeAlert = function(index) {
+        $scope.alertas.splice(index, 1);
+  };
    
 });
